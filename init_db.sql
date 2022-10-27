1b25b7b1c8406a8ad1b498a5032604ed5d511da5
JulesLjs <jules.lejas@gmail.com>
2022-10-27 14:25:15 +0200

    init db

---
+++ init_db.sql
@@ -0,0 +1,38 @@
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+CREATE TABLE profil ( email TEXT PRIMARY KEY, 
+    nom TEXT, 
+    prenom TEXT, 
+    mot_de_passe TEXT, 
+    civilite TEXT, 
+    date_de_naissance DATE) ;
+
+CREATE SEQUENCE seq_id_trajet;
+CREATE TABLE trajet (id INT PRIMARY KEY DEFAULT nextval('seq_id_trajet'), 
+    date DATE, 
+    heure_depart TIME, 
+    ville_depart TEXT, 
+    ville_arrivee TEXT, 
+    numero_train INT, 
+    heure_arrivee TIME, 
+    disponibilite_max BOOL);
+
+CREATE TABLE recherche (
+id INT REFERENCES trajet(id),
+email TEXT REFERENCES profil(email),
+CONSTRAINT pk_recherche PRIMARY KEY(id, email)
+);
+
+
+
+
+INSERT INTO trajet(date, heure_depart, ville_depart, ville_arrivee, numero_train, heure_arrivee, disponibilite_max) VALUES
+('02/03/2021','12:00:00' , 'BRUZ', 'RENNES', 105, '13:00:00', TRUE);
+
+
+INSERT INTO profil(email, nom, prenom, mot_de_passe, civilite, date_de_naissance) VALUES
+('jules@gmail.com', 'Jules', 'Lejas', 'bgdu33', 'F','02/03/2001');
+
+INSERT INTO recherche(email, id) VALUES 
+('jules@gmail.com',1);
