30fda71c6d48a8a6fd3e43cd89b0f376e25babbe
JulesLjs <jules.lejas@gmail.com>
2022-10-27 14:50:45 +0200

    maj app

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-# Import classique
 import uvicorn
 from fastapi import FastAPI
 from DAO.profilDAO import ProfilDAO
-# On instancie le webservice
+
 app = FastAPI()
 
-# Défintion du endpoint get /todo
 @app.get("/profil")
 async def get_profil():
-    # Vous devez récupérer les attaques en base en utilisant votre DAO
     return ProfilDAO().find_by_id(1)
 
 
