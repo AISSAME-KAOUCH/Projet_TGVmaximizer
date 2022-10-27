1834fbbdac7da7ba3914687b5a05f5a84dc7b50a
el <il-yass.el-fikri@eleve.ensai.fr>
2022-10-27 14:51:16 +0200

    modif recherches

---
+++ business_object/recherches/recherche_aller.py
@@ -0,0 +1,26 @@
+from recherche import Recherche
+from  DAO import rechercheDAO 
+
+class Recherche_aller(Recherche):
+
+    def __init__(self, profil: Profil, trajet: Trajet) -> None:
+        super().__init__()
+        self.profil = profil
+        self.trajet = trajet
+
+    def recherche(self):
+        return rechercheDAO.find_by_id(self.trajet)
+    
+    def sauvegarder(self):
+        rechercheDAO.sauvegarder(self.trajet, self.profil)
+
+    def creer_alerte(self, choix):
+        if choix == "oui":
+            rechercheDAO.creer_alerte(self.trajet, self.profil)
+
+    
+    
+    
+        
+
+
