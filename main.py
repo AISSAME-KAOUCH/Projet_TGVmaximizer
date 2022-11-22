9d5107718544eb58cceef0c528ba3ca428aafbac
el <il-yass.el-fikri@eleve.ensai.fr>
2022-11-22 18:49:50 +0100

    aller simple view

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from business_object.recherches.recherche_30j import Recherche_30j
 from business_object.recherches.recherche_aller_retour import Recherche_aller_retour
 if __name__ == '__main__':
 
-    '''
-    # run the StartView
+    
+    #run the StartView
     current_view = StartView()
     
     while current_view:
@@ -19,22 +19,23 @@ if __name__ == '__main__':
         current_view.display_info()
         current_view = current_view.make_choice()
 
-    '''
+    
 
-    profil=Profil('H', 'ali', 'ali', '15/10/2000', 'aissame@gmail.com', '0000')
-    trajet=Trajet(1,'RENNES','02-12-2022','11:51:00','LAVAL','12:48',67,'OUI')
-    trajet2=Trajet(1,'LAVAL','10-12-2022','07:40:00','RENNES','12:48',8,'OUI')
-    #res=Recherche_aller(profil,trajet).recherche()
-    #for trj in res :
-    #    print(trj.__str__())
-    (resultat_req1,resultat_req2)=Recherche_aller_retour(profil,trajet,trajet2).recherche()
-    print(len(resultat_req1),len(resultat_req2))
-    print('\n\n\t\t--------------aller---------------\n\n')
-    for i in resultat_req1 :
-        print(i.__str__())
-    print('--------------retour---------------\n\n')
-    for j in resultat_req2 : 
-        print(j.__str__())      
+
+
+
+    #profil=Profil('H', 'ali', 'ali', '15/10/2000', 'aissame@gmail.com', '0000')
+    #trajet=Trajet(1,'RENNES','02-12-2022','11:51:00','LAVAL','12:48',67,'OUI')
+    #trajet2=Trajet(1,'LAVAL','10-12-2022','07:40:00','RENNES','12:48',8,'OUI')
+    #
+    #(resultat_req1,resultat_req2)=Recherche_aller_retour(profil,trajet,trajet2).recherche()
+    #print(len(resultat_req1),len(resultat_req2))
+    #print('\n\n\t\t--------------aller---------------\n\n')
+    #for i in resultat_req1 :
+     #   print(i.__str__())
+    #print('--------------retour---------------\n\n')
+    #for j in resultat_req2 : 
+     #   print(j.__str__())      
         
 
 
@@ -42,27 +43,27 @@ if __name__ == '__main__':
     #for trj in res :
     #    print(trj.__str__())  
 #PAS DE TEST DANS LE MAIN
-"""
-if __name__=='__main__' :
-    trajet=Trajetclient()
-    test=TrajetDAO()
-    id_ini=test.find_max_id()
-    print(id_ini[0]['maximum'])
+
+#if __name__=='__main__' :
+#    trajet=Trajetclient()
+ #   test=TrajetDAO()
+  #  id_ini=test.find_max_id()
+   # print(id_ini[0]['maximum'])
     
-    resultat=trajet.get_trajets('2022','11','08',"NANTES","LE+MANS",id_ini[0]['maximum'])
+    #resultat=trajet.get_trajets('2022','11','08',"NANTES","LE+MANS",id_ini[0]['maximum'])
     #for tj in resultat :
     #    print(tj.__str__())
 
     
-    test.insert_trajets(resultat)
-    res=test.find_by_depart('2022-11-08','11:05','NANTES','LE MANS')
+    #test.insert_trajets(resultat)
+    #res=test.find_by_depart('2022-11-08','11:05','NANTES','LE MANS')
     #print(res)
 
    
-    for t in res :
-        print(t.__str__())      
+    #for t in res :
+     #   print(t.__str__())      
     #for i in res :
     #    print(i.__str__())
-"""    
+#"""    
 
     
